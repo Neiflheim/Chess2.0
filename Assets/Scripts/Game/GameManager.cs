2879ff1c4c43f7cfb4c7d0509a74eab19fda40f5
Neiflheim <isambart.hugo0912@gmail.com>
2025-01-29 17:58:05 +0100

    Upgrading_AIHandler_introduce_FirstCall

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -53,26 +53,32 @@ namespace Game
 
             if (Input.GetButtonDown("Fire1"))
             {
-                if (_node == null)
-                {
-                    _node = new Node(BoardsHandler.Instance.Pieces, IsWhiteTurn, IsWhiteTurn);
-                    nodes = _node.Children();
-                    Debug.Log("Actual heuristic value : " + _node.HeuristicValue());
-                    Debug.Log("Child number : " + _node.Children().Count);
-                }
+                // Pour tester et voir les enfants
+                
+                // if (_node == null)
+                // {
+                //     _node = new Node(BoardsHandler.Instance.Pieces, IsWhiteTurn, IsWhiteTurn);
+                //     nodes = _node.Children();
+                //     Debug.Log("Actual heuristic value : " + _node.HeuristicValue());
+                //     Debug.Log("Child number : " + _node.Children().Count);
+                // }
+                // _index += 1;
+                // ChangePieces(nodes[_index]);
                 
                 
-                _index += 1;
-                // Debug.Log(_index);
-                ChangePieces(nodes[_index]);
-
-                // int value = _aiHandler.MinMax(_node, 1, true);
-                // BoardsHandler.Instance.Pieces = _aiHandler.bestChild.Pieces;
-                // Debug.Log("MinMax : " + value);
-                //
-                // BoardsHandler.Instance.ResetMatrix();
-                // BoardsHandler.Instance.DisplayMatrix();
-                // Instance.IsWhiteTurn = !Instance.IsWhiteTurn;
+                
+                // Pour tester le MinMax
+                
+                _node = new Node(BoardsHandler.Instance.Pieces, IsWhiteTurn, IsWhiteTurn);
+                nodes = _node.Children();
+                
+                int value = _aiHandler.MinMax(_node, 3, true, true);
+                BoardsHandler.Instance.Pieces = _aiHandler.BestChild.Pieces;
+                Debug.Log("MinMax : " + value);
+                
+                BoardsHandler.Instance.ResetMatrix();
+                BoardsHandler.Instance.DisplayMatrix();
+                Instance.IsWhiteTurn = !Instance.IsWhiteTurn;
             }
         }
 
--- Assets/Scripts/MinMax/AIHandler.cs
+++ Assets/Scripts/MinMax/AIHandler.cs
@@ -5,9 +5,9 @@ namespace MinMax
 {
     public class AIHandler : MonoBehaviour
     {
-        public Node bestChild = null;
+        public Node BestChild = null;
         
-        public int MinMax(Node node, int depth, bool maximizingPlayer)
+        public int MinMax(Node node, int depth, bool maximizingPlayer, bool firstCall)
         {
             if (depth == 0 || node.IsTerminal())
             {
@@ -22,18 +22,20 @@ namespace MinMax
                 List<Node> nodeChildren = node.Children();
                 foreach (Node child in nodeChildren)
                 {
-                    maxHeuristicValue = Mathf.Max(maxHeuristicValue, MinMax(child, depth - 1, false));
+                    maxHeuristicValue = Mathf.Max(maxHeuristicValue, MinMax(child, depth - 1, false, false));
 
-                    if (bestChildHeuristicValue == 0)
+                    if (firstCall)
                     {
-                        bestChildHeuristicValue = maxHeuristicValue;
-                        bestChild = child;
-                    }
-
-                    if (maxHeuristicValue != bestChildHeuristicValue)
-                    {
-                        bestChildHeuristicValue = maxHeuristicValue;
-                        bestChild = child;
+                        if (bestChildHeuristicValue == 0)
+                        {
+                            bestChildHeuristicValue = maxHeuristicValue;
+                            BestChild = child;
+                        }
+                        else if (bestChildHeuristicValue < maxHeuristicValue)
+                        {
+                            bestChildHeuristicValue = maxHeuristicValue;
+                            BestChild = child;
+                        }
                     }
                 }
                 
@@ -46,7 +48,7 @@ namespace MinMax
                 List<Node> nodeChildren = node.Children();
                 foreach (Node child in nodeChildren)
                 {
-                    minHeuristicValue = Mathf.Min(minHeuristicValue, MinMax(child, depth - 1, true));
+                    minHeuristicValue = Mathf.Min(minHeuristicValue, MinMax(child, depth - 1, true, false));
                 }
                 
                 return minHeuristicValue;
